f7b2adcac9432bda2df6a20c61934cdcd6080c71
Oleksii Tuhai <alextugai73@gmail.com>
2025-02-17 22:39:07 +0100

    v 0.1

--- src/index.js
+++ src/index.js
@@ -2,16 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
+
+// npm i json-server
+// npm i browser-router
+// npm i react-redux
+// npm i react-router-dom
+// npm i concurrently
+// npm i @reduxjs/toolkit
+// npm i @fortawesome/react-fontawesome
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
